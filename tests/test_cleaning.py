c1e2656d1f4eabde1a0aa53f4d0e8a1b75cfc54c
Fariza <farizadah@gmail.com>
2025-07-25 16:29:53 +0200

    erreur d'assertion

--- tests/test_cleaning.py
+++ tests/test_cleaning.py
@@ -5,4 +5,4 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from src.preprocessing.text_cleaning import clean_text_spacy_custom_2
 
 def test_clean_text():
-    assert clean_text_spacy_custom_2("Bonjour <b>le</b> monde !") == "bonjour le monde "
+    assert clean_text_spacy_custom_2("Bonjour <b>le</b> monde !") == "bonjour le monde"

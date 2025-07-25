b60daf2ad5f5efa578118aeac54a32d0b64cc3a1
Fariza <farizadah@gmail.com>
2025-07-25 14:00:15 +0200

    Mise en place de GitHub Action

--- tests/test_cleaning.py
+++ tests/test_cleaning.py
@@ -2,7 +2,7 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from src.preprocessing.cleaning import clean_text
+from src.preprocessing.text_cleaning import clean_text_spacy_custom_2
 
 def test_clean_text():
-    assert clean_text("Bonjour <b>le</b> monde !") == "bonjour le monde "
+    assert clean_text_spacy_custom_2("Bonjour <b>le</b> monde !") == "bonjour le monde "
